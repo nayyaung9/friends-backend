ef45d08e109bb869d910400c721bcf2a782190a3
nayyaung9 <naygit.developer@gmail.com>
2021-08-20 11:55:02 +0630

    init app

---
+++ src/loaders/index.ts
@@ -0,0 +1,34 @@
+import expressLoader from './express';
+import dependencyInjectorLoader from './dependencyInjector';
+import mongooseLoader from './mongoose';
+import jobsLoader from './jobs';
+import Logger from './logger';
+import './events';
+
+export default async ({ expressApp }) => {
+  const mongoConnection = await mongooseLoader();
+  Logger.info('✌️ DB loaded and connected!');
+
+  const userModel = {
+    name: 'userModel',
+    // Notice the require syntax and the '.default'
+    model: require('../models/user').default,
+  };
+
+  // It returns the agenda instance because it's needed in the subsequent loaders
+  const { agenda } = await dependencyInjectorLoader({
+    mongoConnection,
+    models: [
+      userModel,
+      // salaryModel,
+      // whateverModel
+    ],
+  });
+  Logger.info('✌️ Dependency Injector loaded');
+
+  await jobsLoader({ agenda });
+  Logger.info('✌️ Jobs loaded');
+
+  await expressLoader({ app: expressApp });
+  Logger.info('✌️ Express loaded');
+};
