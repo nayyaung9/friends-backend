5dc4302fa68874978b2453610892535f8681d46f
nayyaung9 <naygit.developer@gmail.com>
2021-08-25 22:59:54 +0630

    test: redeploying api

--- src/config/index.ts
+++ src/config/index.ts
@@ -3,12 +3,12 @@ import dotenv from 'dotenv';
 // Set the NODE_ENV to 'development' by default
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
-const envFound = dotenv.config();
-if (envFound.error && process.env.NODE_ENV == 'development') {
-  // This error should crash whole process
+dotenv.config();
+// if (envFound.error && process.env.NODE_ENV == 'development') {
+//   // This error should crash whole process
 
-  throw new Error("⚠️  Couldn't find .env file  ⚠️");
-}
+//   throw new Error("⚠️  Couldn't find .env file  ⚠️");
+// }
 
 export default {
   /**
