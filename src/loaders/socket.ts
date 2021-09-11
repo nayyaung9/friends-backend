91365f67fed0f84f7ad2aa5c5c6087df6141688a
nayyaung9 <naygit.developer@gmail.com>
2021-09-11 16:09:39 +0630

    remove auth token

--- src/loaders/socket.ts
+++ src/loaders/socket.ts
@@ -1,15 +1,10 @@
 export default ({ server }) => {
   const io = require('socket.io')(server, {
     rejectUnauthorized: false,
-    // auth: {
-    //   token: 'abc',
-    // },
   });
 
   io.on('connection', socket => {
     console.log('New User Connected');
-    const token = socket.handshake.auth.token;
-    console.log('token', token);
     const { roomId } = socket.handshake.query;
     console.log('socket.handshake', socket.handshake);
     socket.join(roomId);
