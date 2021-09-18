9052ac880c5e7416936f96f0853d93fe802fa995
nayyaung9 <naygit.developer@gmail.com>
2021-09-18 15:33:02 +0630

    feat: listing all active users

--- src/loaders/socket.ts
+++ src/loaders/socket.ts
@@ -4,19 +4,30 @@ export default ({ server }) => {
   });
 
   io.on('connection', socket => {
-    console.log('New User Connected');
+    const users = []; // we record all active users
+
+    for (const [id, socket] of io.of('/').sockets) {
+      users.push({
+        userID: id,
+        username: 'username here',
+      });
+    }
+    socket.emit('users', users);
+
     const { roomId } = socket.handshake.query;
-    console.log('socket.handshake', socket.handshake);
     socket.join(roomId);
 
     socket.on('room', async function (roomId) {
-      console.log(roomId);
       socket.join(roomId);
     });
 
-    socket.on('event://send-message', async function (message) {
-      const data = JSON.parse(message);
-      console.log(data);
+    socket.on('event://send-message', async function (msgObject) {
+      const data = JSON.parse(msgObject);
+
+      socket.to(data.receiver).emit('event://send-message', {
+        content: data.content,
+        from: socket.id,
+      });
     });
   });
 };
